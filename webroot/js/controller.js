939c13a553ecd96c7a6c10c498371a5e54353aec
DavidWyss <dmwyss@gmail.com>
2019-10-05 20:31:53 +1000

    Plants have baby stages and can now grow.

--- webroot/js/controller.js
+++ webroot/js/controller.js
@@ -1,15 +1,36 @@
 
 var bgGrass;
-/*
-var windowSettings = {
-	player: {
-		speedXY: 300
-	}
-};
-*/
-
 let isDebug = true;
 let textDebug;
+
+var settings = {
+	inventory: {
+		plantsAvailable: 4,
+		isAddPlant: function() {
+			//d ebugger;
+			let iNow = this.plantsAvailable;
+			this.plantsAvailable = Math.max(0, this.plantsAvailable - 1);
+			return iNow > 0;
+		}
+	}
+};
+
+var timer = {
+	_isClickOk: true,
+	resetClick: function() {
+		this._isClickOk = true;
+	},
+	canClick: function() {
+		return this._isClickOk;
+	},
+	doClick: function() {
+		this._isClickOk = false;
+		//this.clickTimer = 
+		setTimeout(function(){
+			timer.resetClick();
+		}, 1000);
+	}
+}
 
 var config = {
     type: Phaser.AUTO,
@@ -41,26 +62,28 @@ var config = {
 var controls;
 
 var game = new Phaser.Game(config);
+var scene;
 
 function preload () {
-    this.load.image('bgGrass', '../img/bg.grass.png');
-    this.load.image('mobFlytrap', '../img/mob.flytrap.png');
+	scene = this;
+	scene.load.image('bgGrass', '../img/bg.grass.png');
+	//scene.load.image('mobFlytrapBaby01', '../img/mob.flytrapBaby.01.svg');
+	//scene.load.image('mobFlytrapBaby02', '../img/mob.flytrapBaby.02.svg');
+	//scene.load.image('mobFlytrapBaby03', '../img/mob.flytrapBaby.03.svg');
+	//scene.load.image('mobFlytrap', '../img/mob.flytrap.png');
+	//scene.load.image('mobFlytrap', '../img/mob.flytrap.sprite.svg');
+	this.load.spritesheet("mobFlytrap", '../img/mob.flytrap.sprite.svg', { frameWidth: 275, frameHeight: 275 });
 }
 
-console.log("dsfdsfsdfsdfdsfsdfsdfsdf");
-
 function create () {
-	
-
- 
 	/*
 	TODO Finish this to make cursor a seed... 	cursor: url('../img/cursor.crosshairs.png') 16 16, crosshair;
 	document.body.style.cursor = "url(../img/cursor.seed.png)";
 	*/
 	
-	document.querySelector("*").style.cursor = "url(../img/cursor.crosshairs.png)";
+	//document.querySelector("*").style.cursor = "url(../img/cursor.crosshairs.png)";
 	
-    bgGrass = this.add.tileSprite(0, 0, 4000, 4000, "bgGrass"); //TODO change this to game
+    bgGrass = scene.add.tileSprite(0, 0, 4000, 4000, "bgGrass"); //TODO change this to game
 	bgGrass.fixedToCamera = false;
 	//mobFlytrap = this.add.sprite(300, 300, "mobFlytrap");
 	
@@ -69,33 +92,42 @@ function create () {
 
     this.cameras.main.setBounds(0, 0, 4000, 4000);
 
-    var cursors = this.input.keyboard.createCursorKeys();
+    var cursors = scene.input.keyboard.createCursorKeys();
 
     var controlConfig = {
-        camera: this.cameras.main,
+        camera: scene.cameras.main,
         left: cursors.left,
         right: cursors.right,
         up: cursors.up,
         down: cursors.down,
-        zoomIn: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
-        zoomOut: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
+        zoomIn: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
+        zoomOut: scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
         acceleration: 0.5,
         drag: 0.002,
         maxSpeed: 0.5
     };
 
     controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
-    textDebug = this.add.text(10, 10, 'Move the mouse', { font: '16px Courier', fill: '#00ff00' });
+    textDebug = scene.add.text(10, 10, 'Move the mouse', { font: '16px Courier', fill: '#00ff00' });
     
-    
+    let frX = this.anims.generateFrameNumbers('mobFlytrap', { start: 0, end: 4 });
+    scene.anims.create({ key: 'grow', frames: frX, frameRate: 0.1, repeat: 0 });
+
 }
 
 function update (time, delta) {
-	var pointer = this.input.activePointer;
+	var pointer = scene.input.activePointer;
 	
 	if (pointer.isDown) {
-		//mobFlytrap = this.add.sprite(pointer.midPoint.x, pointer.midPoint.y, "mobFlytrap");
-		mobFlytrap = this.add.sprite(pointer.worldX, pointer.worldY, "mobFlytrap");
+		// If statement nesting required for debugging.
+		if (timer.canClick()) {
+			if (settings.inventory.isAddPlant()) {
+				timer.doClick();
+				//mobFlytrap = this.load.spritesheet(pointer.worldX, pointer.worldY, "mobFlytrap", { frameWidth: 275, frameHeight: 275 });
+				mobFlytrap = scene.add.sprite(pointer.worldX, pointer.worldY, "mobFlytrap");
+				mobFlytrap.anims.play("grow");
+			}    
+		}    
 	}    
 	controls.update(delta);
 
